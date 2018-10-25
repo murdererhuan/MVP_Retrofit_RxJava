9d92a92de60250bfe5d8af84aeadef86bbcb58f0
“haunchen123@163.com <haunchen123@163.com>
2018-10-25 15:51:22 +0800

    添加weather model

--- app/src/main/java/com/huan/mvp_retrofit_rxjava_example/retrofix/ApiService.java
+++ app/src/main/java/com/huan/mvp_retrofit_rxjava_example/retrofix/ApiService.java
@@ -1,4 +1,15 @@
 package com.huan.mvp_retrofit_rxjava_example.retrofix;
 
+import java.util.Observable;
+
+import retrofit2.http.GET;
+
 public class ApiService {
+
+    //baseurl
+    String API_SERVER_URL= "http://apistore.baidu.com/microservice/";
+
+    //加载天气
+    @GET("weather")
+    Observable<>
 }
